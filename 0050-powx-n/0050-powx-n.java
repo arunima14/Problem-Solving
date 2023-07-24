a5aaa96e8f02eed63f06923bcbf63aec0ec3fb49
Arunima Singh <arunimaforwork@gmail.com>
2023-07-24 22:28:19 +0530

    Time: 0 ms (100.00%), Space: 41.1 MB (29.42%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,18 @@
+class Solution {
+    public double myPow(double x, int n) {
+        return Math.pow(x , n);
+//         if(n==0)
+//             return 1;
+//         int abs_N = (int)Math.abs(n);
+//         double pow = x;
+//         while(n>0){
+//             pow = pow * n;
+//             n--;
+//         }
+        
+//         if(n < 0)
+//             return 1/pow;
+//         else
+//             return pow;
+    }
+}
