5282a81bfd81b2f16e96582858c6aab9b2b64722
Arunima Singh <arunimaforwork@gmail.com>
2023-08-07 15:56:01 +0530

    Time: 582 ms (5.03%), Space: 44.2 MB (22.53%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,43 +1,30 @@
+import java.util.*;
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-
-        // String str = "";
-        // int max_len = 0, n = s.length();
-        // for(int i=0; i<n; i++){
-        //     char ch = s.charAt(i);
-        //     String a = "" + ch;
-        //     if(! str.contains(a)){
-        //         str += a;
-        //         int len = str.length();
-        //     for(int j=i+1; j<n; j++){
-        //         char c = s.charAt(j);
-        //         String b = "" + c;
-        //         if(str.contains(b))
-        //         {
-        //             str = str.substring(1, str.length());
-        //         }
-
-        //         str = str + b;
-        //         len = str.length();
-
-        //         max_len = (int)Math.max(len, max_len);
-        //     }
-        //     str="";
-        //     }
-        // }
-
-        // return max_len;
-
-        int n = s.length(), longest = 0;
-        int[] nextIndex = new int[128]; 
-
-        for (int r=0, l=0; r<n; r++) {
-            l = Math.max(nextIndex[s.charAt(r)], l); 
-            longest = Math.max(longest, r - l + 1);
-            nextIndex[s.charAt(r)] = r + 1;
+        if(s.length() < 2)
+            return s.length();
+        int longest = 1;
+        for(int i=0; i<s.length(); i++){
+            List<Character> characters = new ArrayList<>();
+            characters.add(s.charAt(i));
+            int len = 1;
+            
+            for(int j=i; j<s.length()-1; j++){
+                char ch = s.charAt(j+1);
+                
+                if(characters.contains(ch)){
+                    characters.clear();
+                    longest = (int)Math.max(longest, len);
+                    break;
+                }
+                else{
+                    characters.add(ch);
+                    len++;
+                }
+            }
+            longest = (int)Math.max(longest, len);
         }
-
+        
         return longest;
-
     }
 }
