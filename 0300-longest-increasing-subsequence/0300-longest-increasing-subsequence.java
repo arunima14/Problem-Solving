33dad8cf4767f159188a7a498ff722c4c23041cf
Arunima Singh <arunimaforwork@gmail.com>
2023-08-17 23:28:12 +0530

    Time: 13 ms (77.87%), Space: 43.1 MB (70.21%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,42 +1,26 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        // int longest = 1;
-        // for(int i=0; i<nums.length-1; i++){
-        //     if(nums[i+1] > nums[i])
-        //         longest++;
-        // }
-        
-        
-        // return longest;
-        
-        List<Integer> list = new ArrayList<>();
-        
-        for(int i=0;i<nums.length;i++){
-            if(list.isEmpty() || list.get(list.size()-1) < nums[i])
+        int n = nums.length;
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i = 0; i < n; i++){
+            if(list.isEmpty() || list.get(list.size() - 1) < nums[i])
                 list.add(nums[i]);
+            else if(list.contains(nums[i]))
+                continue;
             else{
-                list.set(binSearch(list,nums[i]),nums[i]);
+                int l = 0, r = list.size() -1;
+                while(l <= r){
+                    int mid = (l+r)/2;
+                    if(list.get(mid) == nums[i])
+                        break;
+                    else if(list.get(mid) < nums[i])
+                        l = mid + 1;
+                    else
+                        r = mid - 1;
+                }
+                list.set(l, nums[i]);
             }
         }
-        
         return list.size();
-    }
-    
-    
-    int binSearch(List<Integer> list, int num){
-        int start =0, end = list.size();
-        int index = 0;
-        while(start <= end){
-            int mid = (start+end)/2;
-            if(list.get(mid) == num)
-                return mid;
-            else if(list.get(mid) < num){
-                start = mid+1;
-            }else{
-                index = mid;
-                end =  mid-1;
-            }
-        }
-        return index;
     }
 }
