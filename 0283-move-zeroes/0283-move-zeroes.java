a0f70ffc5ad627274219b5a2008e74522dc9c257
Arunima Singh <arunimaforwork@gmail.com>
2023-08-25 16:12:30 +0530

    Time: 1 ms (100.00%), Space: 45.1 MB (67.51%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,12 +1,10 @@
 class Solution {
     public void moveZeroes(int[] nums) {
         int[] temp = new int[nums.length];
-        int p = 0, count = 0;
+        int p = 0;
         
         for(int i=0; i<nums.length; i++){
-            if(nums[i] == 0)
-                count++;
-            else
+            if(nums[i]!= 0)
                 temp[p++] = nums[i];
         }
         for(int i=p; i<nums.length; i++)
