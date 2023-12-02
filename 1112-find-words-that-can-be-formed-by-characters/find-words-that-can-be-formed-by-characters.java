64a2d6386185c387f4d2adb9785baa15d845a9f8
Arunima Singh <arunimaforwork@gmail.com>
2023-12-02 23:07:11 +0530

    Time: 2 ms (100.00%) | Memory: 43.9 MB (79.63%) - LeetSync

--- 1112-find-words-that-can-be-formed-by-characters/find-words-that-can-be-formed-by-characters.java
+++ 1112-find-words-that-can-be-formed-by-characters/find-words-that-can-be-formed-by-characters.java
@@ -1,22 +1,29 @@
 class Solution {
-  public int countCharacters(String[] words, String chars) {
-    var map = new int[26];
-    var sum = 0;
-
-    for (var c : chars.toCharArray()) map[c - 'a']++;
-
-    for (var word : words) {
-      var map2 = map.clone();
-      var valid = true;
-
-      for (var c : word.toCharArray()) {
-        if (--map2[c - 'a'] < 0) {
-          valid = false;
-          break;
+    public int countCharacters(String[] words, String chars) {
+        int[] counts = new int[26];
+        for(int i=0; i<chars.length(); i++)
+        {
+            counts[chars.charAt(i) - 'a']++;
         }
-      }
-      if (valid) sum += word.length();
+        int count = 0 ;
+        for(String s : words)
+        {
+            if(isWordPossible(s,counts))
+                count += s.length();
+        }
+        return count;
     }
-    return sum;
-  }
+
+    boolean isWordPossible(String word, int[] counts)
+    {
+        int[] c = new int[26];
+        for(int i=0; i<word.length(); i++)
+        {
+            int x = word.charAt(i) - 'a';
+            c[x]++;
+            if(c[x] > counts[x])
+                return false;
+        }
+        return true;
+    }
 }
